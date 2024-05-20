333ca1ec098de9ab404f84e234add114ea65f664
sudip <paudelsudip134@gmail.com>
2024-05-20 13:39:55 +0545

    emailSender updated

--- Backend/controller/email/emailSender.js
+++ Backend/controller/email/emailSender.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer')
 const dotenv = require('dotenv')
 dotenv.config();
-const sendMail = async (to,subject,text) => {
+const sendMail = async (to,subject,html) => {
     try {
         //transporter object 
         let transporter = nodemailer.createTransport({
@@ -12,12 +12,12 @@ const sendMail = async (to,subject,text) => {
             }
         });
 
-        // Set up email data
+        // Email data
         let mailOptions = {
             from: process.env.EMAIL_USERNAME,
             to: to,
             subject: subject,
-            text: text
+            html: html
         };
 
         // Send email
@@ -29,39 +29,5 @@ const sendMail = async (to,subject,text) => {
         return 'error'
     }
 };
-// const sendMail = async (req, res) => {
-//     const data = req.body.emailData;
-//     const emailData = {
-//         to: data.to,
-//         subject: data.subject,
-//         text: data.text
-//     };
-//     try {
-//         //transporter object 
-//         let transporter = nodemailer.createTransport({
-//             service: 'gmail',
-//             auth: {
-//                 user: process.env.EMAIL_USERNAME, 
-//                 pass: process.env.EMAIL_PASSWORD 
-//             }
-//         });
-
-//         // Set up email data
-//         let mailOptions = {
-//             from: process.env.EMAIL_USERNAME,
-//             to: emailData.to,
-//             subject: emailData.subject,
-//             text: emailData.text
-//         };
-
-//         // Send email
-//         let info = await transporter.sendMail(mailOptions);
-//         // console.log('Message sent: %s', info.messageId);
-//         res.status(200).send('success');
-//     } catch (error) {
-//         // console.error('Error sending email:', error);
-//         res.status(500).send('error');
-//     }
-// };
 
 export default sendMail;
