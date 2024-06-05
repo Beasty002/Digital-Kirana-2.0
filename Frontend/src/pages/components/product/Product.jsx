0e0c60ae3ed1098c042c6732cb17bc00a34bb111
Beasty002 <xasbin12@gmail.com>
2024-06-05 15:03:45 +0545

    fix css

--- Frontend/src/pages/components/product/Product.jsx
+++ Frontend/src/pages/components/product/Product.jsx
@@ -2,43 +2,43 @@
 
 import { Link } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
-import { addToCart} from '../../../store/cartSlice'
+import { addToCart } from '../../../store/cartSlice'
 
-const Product = ({data,change}) => {
+const Product = ({ data, change }) => {
   const dispatch = useDispatch()
   const handleData = () => {
     change(data)
   }
 
-  const handleAddToCart = product =>{
-      dispatch(addToCart(product))
+  const handleAddToCart = product => {
+    dispatch(addToCart(product))
   }
 
-  
+
   // console.log(data)
   return (
     <>
-    <div  className="product-card">
-      <Link to={`/singlePage/${data._id}`}  onClick={handleData} >
-        <figure className="product-img-container">
-          <img src= {`../../Assets/Images/Products/${data.frontView}`}
-            alt="" />
-        </figure>
+      <div className="product-card">
+        <Link to={`/singlePage/${data._id}`} onClick={handleData} >
+          <figure className="product-img-container">
+            <img src={`../../Assets/Images/Products/${data.frontView}`}
+              alt="" />
+          </figure>
+        </Link>
 
         <h3 className="product-name">{data.productName} </h3>
         <span className="product-price">Rs:{data.salesPrice}</span>
-      </Link>
         <div className="product-qty-changer">
-          <span >Qty</span> 
-           <div className="qty-btn">
+          <span >Qty</span>
+          <div className="qty-btn">
             <button className="decrease" >-</button>
             <span>00</span>
             <button className="increase">+</button>
           </div>
         </div>
-        <button className="add-to-cart-btn" onClick={()=> handleAddToCart(data)}><box-icon name='cart' ></box-icon>Add to Cart</button>
+        <button className="add-to-cart-btn" onClick={() => handleAddToCart(data)}><box-icon name='cart' ></box-icon>Add to Cart</button>
 
-    </div>
+      </div>
     </>
   )
 }
